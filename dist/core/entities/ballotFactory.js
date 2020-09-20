bb787ca9ca2509733797e57d802ca2a407d4e2bf
Benjamin Bohec <bbohec.pro@gmail.com>
2020-09-20 18:00:37 +0200

    remove dist

--- dist/core/entities/ballotFactory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ballotFactory = void 0;
-exports.ballotFactory = {
-    isBallotExist(subject) { return ballotFactoryRepository.some(ballot => ballot.subject === subject); },
-    generateBallot(subject) { ballotFactoryRepository.push({ subject }); }
-};
-const ballotFactoryRepository = [];
-//# sourceMappingURL=ballotFactory.js.map
