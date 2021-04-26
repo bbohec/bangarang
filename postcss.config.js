a78f36436b2071d30c688cdaad25152e308662eb
Benjamin Bohec <bbohec.pro@gmail.com>
2021-04-26 19:18:25 +0200

    remove prefixer postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 const tailwind = require('tailwindcss');
 const cssnano = require('cssnano');
 const postcssImport = require('postcss-import');
-const autoprefixer = require('autoprefixer')
 const presetEnv = require('postcss-preset-env')({
   features: {
     'nesting-rules': true, // Optional, not necessary. Read details about it  [here](https://tabatkins.github.io/specs/css-nesting/#motivation) 
@@ -9,6 +8,6 @@ const presetEnv = require('postcss-preset-env')({
 });
 const plugins =
   process.env.NODE_ENV === 'production'
-    ? [postcssImport, tailwind,autoprefixer, presetEnv, cssnano]
-    : [postcssImport, tailwind,autoprefixer, presetEnv];
+    ? [postcssImport, tailwind, presetEnv, cssnano]
+    : [postcssImport, tailwind, presetEnv];
 module.exports = { plugins };
